c3f6a06109bd8d62000a63f5cb047ff2541e2b77
tahurmalik4-blip <tahurmalik4@gmail.com>
2026-02-04 00:22:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: music: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3", // Music streaming URL
+        musicUrl: music: music: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3?v=2",// Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
